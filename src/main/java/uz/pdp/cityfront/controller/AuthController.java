7343ea5876b8e0c83664fd8050e866cf8f9c0cf9
Bilol665 <bilolsultan665@gmail.com>
2023-09-04 11:44:27 +0500

    Initial commit

---
+++ src/main/java/uz/pdp/cityfront/controller/AuthController.java
@@ -0,0 +1,16 @@
+package uz.pdp.cityfront.config.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/fr")
+@RequiredArgsConstructor
+public class AuthController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
---
+++ src/test/java/uz/pdp/cityfront/CityFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.cityfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
