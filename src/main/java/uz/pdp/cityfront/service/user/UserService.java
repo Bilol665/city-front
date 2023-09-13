368bd15fb31f3cb3ab54ff8731a71f8499281f4c
Bilol665 <bilolsultan665@gmail.com>
2023-09-13 13:51:02 +0500

    Reset password done!

--- src/main/java/uz/pdp/cityfront/service/user/UserService.java
+++ src/main/java/uz/pdp/cityfront/service/user/UserService.java
@@ -10,15 +10,20 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 import uz.pdp.cityfront.domain.dto.LoginDto;
+import uz.pdp.cityfront.domain.dto.ResetPasswordDto;
 import uz.pdp.cityfront.domain.dto.UserRequestDto;
 import uz.pdp.cityfront.domain.dto.VerificationDto;
 import uz.pdp.cityfront.domain.dto.reader.JwtResponse;
 import uz.pdp.cityfront.domain.dto.reader.UserReadDto;
+import uz.pdp.cityfront.domain.dto.response.ApiResponse;
 import uz.pdp.cityfront.domain.dto.response.ApiResponse4Jwt;
 import uz.pdp.cityfront.domain.entity.token.JwtTokenEntity;
 import uz.pdp.cityfront.exceptions.MyException;
 import uz.pdp.cityfront.repository.JwtTokenRepository;
+import uz.pdp.cityfront.service.mail.MailService;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
@@ -26,6 +31,7 @@ import java.util.UUID;
 public class UserService {
     private final RestTemplate restTemplate;
     private final JwtTokenRepository jwtTokenRepository;
+    private final MailService mailService;
     @Value("${services.user-service}")
     private String userServiceUrl;
 
@@ -87,11 +93,42 @@ public class UserService {
         HttpEntity<String> entity = new HttpEntity<>(headers);
         return restTemplate.exchange(builder.toUriString(), HttpMethod.GET, entity, UserReadDto.class).getBody();
     }
+    public UserReadDto getUserById(String username) {
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(userServiceUrl + "/api/v1/get/user")
+                .queryParam("username", username);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        return restTemplate.exchange(builder.toUriString(), HttpMethod.GET, entity, UserReadDto.class).getBody();
+    }
     private JwtResponse refreshJwt(LoginDto loginDto) {
         UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(userServiceUrl + "/api/v1/auth/login");
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<LoginDto> entity = new HttpEntity<>(loginDto, headers);
         return restTemplate.exchange(builder.toUriString(), HttpMethod.GET, entity, JwtResponse.class).getBody();
+    }
+    public void sendReset(String email) {
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(userServiceUrl + "/api/v1/auth/reset-password/" + email);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        restTemplate.exchange(builder.toUriString(), HttpMethod.GET, entity, JwtResponse.class);
+    }
+
+    public Map<String,String> passwordReset(String email, Integer id) {
+        if(id != 1) throw new MyException("Not your link!");
+        Map<String, String> map = new HashMap<>();
+        map.put("email",email);
+        map.put("path","/auth/passwordReset");
+        return map;
+    }
+
+    public void reset(String email, ResetPasswordDto password) {
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(userServiceUrl + "/api/v1/auth/changePassword/" + email);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<ResetPasswordDto> entity = new HttpEntity<>(password,headers);
+        restTemplate.exchange(builder.toUriString(),HttpMethod.PUT,entity, ApiResponse.class);
     }
 }
